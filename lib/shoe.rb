a15b22687eed6fe4eb03c52c01ba2b041970039e
Ariel Zhen <zhenpingping@live.com>
2018-05-31 20:48:21 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,8 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-  BRANDS = [] 
-  
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
   end
@@ -13,9 +13,9 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-  def brand=(brand) 
-    @brand = brand 
-    BRANDS << brand 
+  def brand=(brand)
+    @brand = brand
+    BRANDS << brand
   end 
 
 end
